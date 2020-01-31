f4a486d27f32863f57255cb539f22ce5bd9482bd
Sandra Alvarez <sandruskiag@gmail.com>
2020-01-31 22:46:30 +0100

    Screwdriver and object keyboard controls

---
+++ Global Game Jam 2020/Assets/Scripts/ObjectController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    #region PUBLIC_VARIABLES
+    public float rotateDegrees;
+    public float interpolateSeconds;
+    #endregion
+
+    #region PRIVATE_VARIABLES
+    private Quaternion fromRotation;
+    private Quaternion toRotation;
+    private float timer;
+    private bool interpolate;
+    #endregion
+
+    void Start()
+    {
+        fromRotation = Quaternion.identity;
+        toRotation = Quaternion.identity;
+        timer = 0.0f;
+        interpolate = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            fromRotation = transform.rotation;
+            toRotation = transform.rotation * Quaternion.AngleAxis(rotateDegrees, Vector3.up);
+            timer = 0.0f;
+            interpolate = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            fromRotation = transform.rotation;
+            toRotation = transform.rotation * Quaternion.AngleAxis(rotateDegrees, -Vector3.up);
+            timer = 0.0f;
+            interpolate = true;
+        }
+
+        if (interpolate)
+        {
+            float t = timer / interpolateSeconds;
+            transform.rotation = Quaternion.Slerp(fromRotation, toRotation, t);
+
+            if (timer >= interpolateSeconds)
+            {
+                interpolate = false;
+            }
+            else
+            {
+                timer += Time.deltaTime;
+            }
+        }
+    }
+}
---
+++ Global Game Jam 2020/Assets/Scripts/ScrewdriverController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrewdriverController : MonoBehaviour
+{
+    #region PUBLIC_VARIABLES
+    public uint totalHeights;
+    public float heightDistance;
+    public float interpolateSeconds;
+    #endregion
+
+    #region PRIVATE_VARIABLES
+    private Vector3 fromPosition;
+    private Vector3 toPosition;
+    private float timer;
+    private bool interpolate;
+    private uint currentHeight;
+    #endregion
+
+    void Start()
+    {
+        fromPosition = Vector3.zero;
+        toPosition = Vector3.zero;
+        timer = 0.0f;
+        interpolate = false;
+        currentHeight = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            uint desiredHeight = currentHeight + 1;
+            if (desiredHeight >= 0 && desiredHeight < totalHeights)
+            {
+                fromPosition = transform.position;
+                toPosition = transform.position + new Vector3(0.0f, heightDistance, 0.0f);
+                timer = 0.0f;
+                interpolate = true;
+                currentHeight = desiredHeight;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            uint desiredHeight = currentHeight - 1;
+            if (desiredHeight >= 0 && desiredHeight < totalHeights)
+            {
+                fromPosition = transform.position;
+                toPosition = transform.position - new Vector3(0.0f, heightDistance, 0.0f);
+                timer = 0.0f;
+                interpolate = true;
+                currentHeight = desiredHeight;
+            }
+        }
+
+        if (interpolate)
+        {
+            float t = timer / interpolateSeconds;
+            transform.position = Vector3.Lerp(fromPosition, toPosition, t);
+
+            if (timer >= interpolateSeconds)
+            {
+                interpolate = false;
+            }
+            else
+            {
+                timer += Time.deltaTime;
+            }
+        }
+    }
+}
