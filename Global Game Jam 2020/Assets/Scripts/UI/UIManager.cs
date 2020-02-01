c1b3479516c9950d645c56c608aa1c0bbb009dee
Guillem Costa Miquel <kayter72@gmail.com>
2020-02-01 11:11:33 +0100

    menu

--- Global Game Jam 2020/Assets/Scripts/UI/UIManager.cs
+++ Global Game Jam 2020/Assets/Scripts/UI/UIManager.cs
@@ -1,15 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject menu;
     public ObjectController objectController;
     public Text text;
     // Update is called once per frame
     void Update()
     {
         text.text = Mathf.CeilToInt(objectController.GameplayTimer).ToString();
+    }
+
+    public void Play()
+    {
+        menu.SetActive(false);
+    }
+
+    public void Howtoplay()
+    {
+
+    }
+
+    public void Exit()
+    {
+
+    }
+
+    public void Credits()
+    {
+
     }
 }
