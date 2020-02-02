81ba849c1665a869809965e3c35567b881e72402
Guillem Costa Miquel <kayter72@gmail.com>
2020-02-02 12:57:11 +0100

    adssad

--- Global Game Jam 2020/Assets/Scripts/UI/HowToPlayManager.cs
+++ Global Game Jam 2020/Assets/Scripts/UI/HowToPlayManager.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class HowToPlayManager : MonoBehaviour
 {
- 
+    InputManager input;
+    private void Start()
+    {
+        input = GetComponent<InputManager>();
+    }
+    private void Update()
+    {
+        if (input.GetButtonDown(InputManager.Gamepads.Gamepad_1, InputManager.Buttons.B))
+        {
+            OnClickBackButton();
+        }
+    }
+
     public void OnClickBackButton()
     {
         SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
